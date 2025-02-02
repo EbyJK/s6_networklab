ff7d7b1e5c17ccaa429ea380152fc91cb89211b8
Eby Joshy Kavungal <ebyjkavungal@gmail.com>
2025-02-02 22:20:20 +0530

    second commit

--- stwserver.c
+++ stwserver.c
@@ -14,6 +14,29 @@ int addrlen=sizeof(address);
 char buffer[BUFFER_SIZE]={0};
 int expected_frame=0;
 
-if ((sock=))
+if ((sock=socket(AF_INET,SOCK_STREAM,0))==0){
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+}
+
+address.sin_family=AF_INET;
+address.sin_addr.s_addr=INADDR_ANY;
+address.sin_port=htons(PORT);
+
+if(bind(sock,(struct sockaddr *)&address,sizeof(address))<0){
+    perror("Bind failed");
+    close(sock);
+    exit(EXIT_FAILURE);
+}
+if(listen(sock,3)<0){
+    perror("listen failed");
+    close(sock);
+    exit(EXIT_FAILURE);
+
+}
+printf("receiver: connection established\n");
+while(1){
+    
+}
 
 }
